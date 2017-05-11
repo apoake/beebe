50804a06f3bbe9a09bce00dc42878e174f9c734e
joey.zhou <justimkiss@gmail.com>
2017-05-11 13:51:25 +0800

    push

--- model/user_model.go
+++ model/user_model.go
@@ -30,16 +30,36 @@ func (User) TableName() string {
 	return "user"
 }
 
+type UserSettings struct {
+	Model
+	ID 				int64		`gorm:"primary_key"`
+	UserId 			int64		`gorm:"column:user_id" json:"userId"`
+	Key 			string		`gorm:"column:key" json:"key"`
+	Val 			string		`json:"val"`
+}
+
+func (UserSettings) TableName() string {
+	return "user_settings"
+}
 
 type Role struct {
 	Model
 	ID 			int64	`gorm:"primary_key" json:"id"`
-	RoleName	string	`grom:"column:"role_name" json:"roleName"`
+	RoleName	string	`grom:"column:role_name" json:"roleName"`
 }
 
 func (Role) TableName() string {
 	return "role"
 }
+
+type RoleUser struct {
+	Model
+	ID 				int64		`gorm:"primary_key" json:"id"`
+	UserId 			int64		`grom:"column:user_id" json:"userId"`
+	TeamId			int64		`grom:"column:team_id" json:"teamId"`
+	RoleId			int64		`grom:"column:role_id" json:"roleId"`
+}
+
 
 type TeamUser struct {
 	Model
@@ -51,17 +71,6 @@ type TeamUser struct {
 
 func (TeamUser) TableName() string {
 	return "team_user"
-}
-
-type UserSetting struct {
-	Model
-	UserId 		int64		`grom:"column:"user_id" json:"userId"`
-	key 		string		`grom:"column:"key" json:"key"`
-	val 		string		`grom:"column:"val" json:"val"`
-}
-
-func (UserSetting) TableName() string {
-	return "user_settings"
 }
 
 type Team struct {
