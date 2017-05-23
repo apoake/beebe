ba58001935e59bd8e49974a213ae830553abb094
joey.zhou <justimkiss@gmail.com>
2017-05-24 02:07:40 +0800

    push

--- server.go
+++ server.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"beebe/model"
-	"fmt"
+	"beebe/controller"
+	"beebe/service"
+	"beebe/config"
+	"net/http"
 )
 
 func main() {
-	//http.ListenAndServe("0.0.0.0:" + config.GetConfig().Web.Port, controller.Macaron())
-	//defer service.DB().Close()
+	http.ListenAndServe("0.0.0.0:" + config.GetConfig().Web.Port, controller.Macaron())
+	defer service.DB().Close()
 
 	//str := "hello_@str(@num(2,3), @num(5,10), lower)_@str(3, 3)"
 	//str := "hello_@str(1, 10, lower)_@str(3,3)"
-	str := "hello_@stre(@num(1,1), @num(3,3), <index@num(1,100)>)"
+	//str := "hello_@stre(@num(1,1), @num(3,3), <index@num(1,100)>)"
 	//str := "hello_@stre(@num(2,2),@num(3,5),@str(3,@num(3,6)))"
-	result, err := model.GetMockManager().MockData(&str)
-	if err != nil {
-		println(err)
-	}
-	fmt.Printf("%v", result)
+	//result, err := model.GetMockManager().MockData(&str)
+	//if err != nil {
+	//	println(err)
+	//}
+	//fmt.Printf("%v", result)
 }
